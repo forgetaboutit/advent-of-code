449c8d1e59a048efab64eead4173ddeffe8a1c0a
Samuel Schuhmacher <s.schuhmacher@digi-zeiterfassung.de>
2022-12-02 11:45:00 +0100

    Add solution to 2022-02 puzzles

--- 2022/app/Main.hs
+++ 2022/app/Main.hs
@@ -1,16 +1,20 @@
 module Main (main) where
 
 import Day1 qualified
+import Day2 qualified
 
 main :: IO ()
 main = do
   runDay 1 Day1.run
+  runDay 2 Day2.run
 
 runDay :: Int -> IO () -> IO ()
 runDay day run = do
   putStrLn dayText
   putStrLn dayUnderline
   run
+  putStrLn ""
+
   where
     dayText = "Day " ++ show day
     dayUnderline = replicate (length dayText) '='
---
+++ 2022/src/Day2.hs
@@ -0,0 +1,94 @@
+module Day2 (run) where
+
+import Data.Text qualified as T
+import Data.Text.IO qualified as TIO
+
+run :: IO ()
+run = do
+  input <- readInput
+
+  let scoreMoves = parseAndEval parseResponseMove input
+  putStrLn $ "Total score part 1: " ++ show scoreMoves
+
+  let scoreOutcomes = parseAndEval parseOutcome input
+  putStrLn $ "Total score part 2: " ++ show scoreOutcomes
+  where
+    parseAndEval parser input = sum $ evaluateRowScore <$> parseInput parser input
+
+evaluateRowScore :: EvalGame a => (Move, a) -> Int
+evaluateRowScore = uncurry eval
+
+moveScore :: Move -> Int
+moveScore Rock = 1
+moveScore Paper = 2
+moveScore Scissors = 3
+
+roundScore :: Move -> Move -> Int
+roundScore Rock Paper = 6
+roundScore Paper Scissors = 6
+roundScore Scissors Rock = 6
+roundScore x y | x == y = 3
+roundScore _ _ = 0
+
+class EvalGame op where
+  eval :: Move -> op -> Int
+
+instance EvalGame Outcome where
+  eval :: Move -> Outcome -> Int
+  eval opMove outcome = moveScore ownMove + roundScore opMove ownMove
+    where
+      ownMove = case (opMove, outcome) of
+        (move, Draw) -> move
+        (Rock, Win) -> Paper
+        (Rock, Loss) -> Scissors
+        (Paper, Win) -> Scissors
+        (Paper, Loss) -> Rock
+        (Scissors, Win) -> Rock
+        (Scissors, Loss) -> Paper
+
+instance EvalGame Move where
+  eval :: Move -> Move -> Int
+  eval opMove ownMove = moveScore ownMove + roundScore opMove ownMove
+
+parseInput :: (T.Text -> a) -> T.Text -> [(Move, a)]
+parseInput parseSnd input = parseInputRow parseSnd <$> T.splitOn "\n" input
+
+parseInputRow :: (T.Text -> a) -> T.Text -> (Move, a)
+parseInputRow parseSnd row = (firstMove, response)
+  where
+    parts = T.splitOn " " row
+    firstMove = parseFirstMove $ head parts
+    response = parseSnd $ parts !! 1
+
+data Move
+  = Rock
+  | Paper
+  | Scissors
+  deriving (Show, Eq, Enum)
+
+data Outcome
+  = Loss
+  | Draw
+  | Win
+  deriving (Show, Eq)
+
+parseFirstMove :: T.Text -> Move
+parseFirstMove "A" = Rock
+parseFirstMove "B" = Paper
+parseFirstMove "C" = Scissors
+parseFirstMove unknown = error $ "Undefined move: '" ++ show unknown ++ "'"
+
+parseResponseMove :: T.Text -> Move
+parseResponseMove "X" = Rock
+parseResponseMove "Y" = Paper
+parseResponseMove "Z" = Scissors
+parseResponseMove unknown = error $ "Undefined move: '" ++ show unknown ++ "'"
+
+parseOutcome :: T.Text -> Outcome
+parseOutcome "X" = Loss
+parseOutcome "Y" = Draw
+parseOutcome "Z" = Win
+parseOutcome unknown = error $ "Undefined outcome: '" ++ show unknown ++ "'"
+
+readInput :: IO T.Text
+readInput = TIO.readFile "./inputs/day2.txt"
